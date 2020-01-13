d319b5f9ef3c2b47de10c72b05bb77f62a27ab39
koogk7 <koogk7@gmail.com>
2020-01-13 14:48:10 +0900

    watch test

--- watchAPI/main.go
+++ watchAPI/main.go
@@ -4,8 +4,9 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	v1 "k8s.io/api/core/v1"
+	apps_v1beta1 "k8s.io/api/apps/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
@@ -37,30 +38,48 @@ func main() {
 
 	label := "app=products"
 	namespace := "default"
-	watch, err := clientset.CoreV1().Pods(namespace).Watch(metav1.ListOptions{
+
+	watch, err := clientset.AppsV1beta1().Deployments(namespace).Watch(metav1.ListOptions{
 		LabelSelector: label,
 	})
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	go func() {
-		for event := range watch.ResultChan() {
-			fmt.Printf("Type: %v\n", event.Type)
-			p, ok := event.Object.(*v1.Pod)
-			if !ok {
-				log.Fatal("unexpected type")
-			}
-			fmt.Println(p.Status.ContainerStatuses)
-			fmt.Println(p.Status.Phase)
-		}
-		done <- true
-	}()
+	list, err := clientset.AppsV1beta1().Deployments(namespace).List(metav1.ListOptions{
+		LabelSelector: label,
+	})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	resultList(*list)
+	go resultWatch(watch, done)
 	<-done
+}
+
+func resultWatch(watch watch.Interface, done chan<- bool) {
+	log.Println("result watch")
+	for event := range watch.ResultChan() {
+		fmt.Printf("Type: %v\n", event.Type)
+		eventObject, ok := event.Object.(*apps_v1beta1.Deployment)
+		if !ok {
+			log.Fatal("unexpected type")
+		}
+		prettyPrint(eventObject.Status)
+	}
+	done <- true
+}
+
+func resultList(list apps_v1beta1.DeploymentList) {
+	log.Println("result list")
+	for _, item := range list.Items {
+		prettyPrint(item.Status)
+	}
 }
 
 func prettyPrint(i interface{}) string {
 	s, _ := json.MarshalIndent(i, "", "\t")
-	print(string(s))
+	println(string(s))
 	return string(s)
 }
